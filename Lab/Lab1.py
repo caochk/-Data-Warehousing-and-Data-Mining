e9505d3b130b37d259c63d395a43e4bae6170537
LoyZowu <chenkai@outlook.it>
2021-02-21 10:33:59 +0800

    version 1

--- Lab/Lab1.py
+++ Lab/Lab1.py
@@ -1,16 +1,3 @@
-# def mySqrt(x):
-#     l, r, ans = 0, x, -1
-#     while l <= r:
-#         mid = (l + r) // 2
-#         if mid * mid <= x:
-#             ans = mid
-#             l = mid + 1
-#         else:
-#             r = mid - 1
-#     print(ans)
-#
-# mySqrt(-1)
-
 import math
 
 def mySqrt(x):
