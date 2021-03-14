49b62612e700621a8846465402e490b91d5d2294
Jason <60861654+LoyZowu@users.noreply.github.com>
2021-03-14 12:26:49 +0800

    version 2
    Complete the function processing cuboid which is not single tuple.

--- Lab/submission-lab2.py
+++ Lab/submission-lab2.py
@@ -16,7 +16,12 @@ def buc_rec_optimized(df):# do not change the heading of the function
         # print(df)
         single_tuple_optimization(df)
     else:
-        buc_rec_optimization()
+        row = []
+        index = []
+        columns = list(baseCuboid.iloc[:, :])
+        resultCuboid = pd.DataFrame(columns=columns)
+        buc_rec_optimization(df, row, index, resultCuboid)
+        # print(resultCuboid)
 
 
 def single_tuple_optimization(baseCuboid):
@@ -64,13 +69,36 @@ def single_tuple_optimization(baseCuboid):
 
     # print(rows)
     # print(resultCuboid)
+    # print(len(baseCuboid.columns))
 
     resultCuboid[baseCuboid.columns[-1]] = baseCuboid.iloc[0, -1]
-    print(resultCuboid)
+    # print(resultCuboid)
 
-def buc_rec_optimization(cuboid):
-    pass
+def base_case(originalCuboid, row, index, resultCuboid):
+    sumOfMeasureValues = originalCuboid.iloc[:, 0].sum()
+    row.append(sumOfMeasureValues)
+    resultCuboid.loc[str(len(index))] = row
+
+def buc_rec_optimization(originalCuboid, row, index, resultCuboid):
+    if len(originalCuboid.columns) == 1:
+        base_case(originalCuboid, row, index, resultCuboid)
+        row.pop()
+    else:
+        firstDimValus = set(originalCuboid.iloc[:, 0])
+
+        for value in firstDimValus:
+            row.append(value)
+            subCuboid = helper.slice_data_dim0(originalCuboid, value)
+            buc_rec_optimization(subCuboid, row, index, resultCuboid)
+            row.pop()
+            index.append(1)
+
+        row.append("ALL")
+        subCuboid = helper.remove_first_dim(originalCuboid)
+        buc_rec_optimization(subCuboid, row, index, resultCuboid)
+        row.pop()
+        # index.append(1)
 
 
-baseCuboid = read_data('c_.txt')
+baseCuboid = read_data('a_.txt')
 buc_rec_optimized(baseCuboid)
